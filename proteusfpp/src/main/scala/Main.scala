fcd8e8070c5cd28530db7638aa5e081fb71aae22
Chance Chime <cchime82@icloud.com>
2024-04-18 01:45:30 -0700

    Refactor Main.scala to improve file handling and code generation

--- proteusfpp/src/main/scala/Main.scala
+++ proteusfpp/src/main/scala/Main.scala
@@ -1,17 +1,46 @@
 import TokenizationTools._
 import ParsingTools._
+import CodeGenerationTools._
+import CodeGenerationTools.SyntaxGenerator._
 import scala.io.Source
 import java.io.{File, PrintWriter}
+import scala.util.{Try, Using}
+import scala.annotation.varargs
+import java.io.PrintWriter
+import java.io.File
+import scala.util.Try
+import java.nio.file.{Files, Path, Paths, StandardCopyOption}
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import scala.util.{Failure, Success}
 
-@main def Main(): Unit =
-  val filePath = "src/main/scala/testInputV2.txt"
-  val fileContent = Source.fromFile(new File(filePath)).toList
-  val tokens = Tokenizer.tokenize(fileContent)
-  println(tokens)
+object Main{
+  def main(args: Array[String]): Unit = {
+    val inputFile = args.headOption.getOrElse("defaultfile.txt")
+    try {
+      val fileContent = Source.fromFile(new File(inputFile)).toList
+      val tokens = Tokenizer.tokenize(fileContent)
+      println(tokens)
 
-  val (proteus, fpp) = Parser.parse(tokens, List.empty, List.empty)
-  print("THIS IS PROTEUS \n")
-  print(proteus)
-  print("\n\n")
-  print("THIS IS FPP \n")
-  print(fpp)
+      val (proteus, fpp) = Parser.parse(tokens, List.empty, List.empty)
+      print("THIS IS PROTEUS \n")
+      print(proteus)
+      print("\n\n")
+      print("THIS IS FPP \n")
+      print(fpp)
+  
+
+    val folderName = generateUniqueFolderName()
+    val folder = Paths.get(folderName)
+    Files.createDirectories(folder)
+    val proteusFileName = "proteusfile.proteus"
+    val fppFileName = "fppfile.fpp"
+    generateFiles(proteus, s"$folderName/$proteusFileName")
+    generateFiles(fpp, s"$folderName/$fppFileName")
+
+    println(s"Files generated and placed in folder $folderName successfully.")
+        } catch {
+      case e: Exception => println(s"Error reading file: ${e.getMessage}")
+    }
+  }
+}
